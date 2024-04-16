6e33cb294ebfccff05bd2268414ca4f181990683
abhishek.kasera1 <abhishek.kasera@fiftyfivetech.io>
2024-04-16 14:02:16 +0530

    countNegatives

---
+++ dsa_practice.js
@@ -0,0 +1,38 @@
+function countNegatives(grid) {
+    const rows = grid.length;
+    const cols = grid[0].length;
+    let count = 0;
+    
+    // Start from the bottom-left corner
+    let row = rows - 1;
+    let col = 0;
+    
+    while (row >= 0 && col < cols) {
+        if (grid[row][col] < 0) {
+            // If the current element is negative, increment count
+            count += cols - col;
+            // Move to the next row
+            row--;
+        } else {
+            // If the current element is non-negative, move to the next column
+            col++;
+        }
+    }
+    
+    return count;
+}
+
+// Test cases
+const grid1 = [
+    [4,3,2,-1],
+    [3,2,1,-1],
+    [1,1,-1,-2],
+    [-1,-1,-2,-3]
+];
+console.log(countNegatives(grid1)); // Output: 8
+
+const grid2 = [
+    [3,2],
+    [1,0]
+];
+console.log(countNegatives(grid2)); // Output: 0
